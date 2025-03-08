b8ad5d64717101b7511fd1f7b701eaa494a847b2
rksvw <ritik.vw98@gmail.com>
2025-03-08 19:24:50 +0530

    completed the postgres database all data retrive

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
+from time import sleep
+import os
+from dotenv import load_dotenv, dotenv_values
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg2 as pypg
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+
+load_dotenv()
+
+PASS = os.getenv("PASSWORD")
 
 
 # Schema
@@ -13,6 +22,25 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+
+while True:
+
+    try:
+        conn = pypg.connect(
+            host="localhost",
+            database="fastApi",
+            user="postgres",
+            password=PASS,
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print(f"Erro: {error}")
+        sleep(2)
 
 
 # Here I learn how to use FastAPI and some CRUD operation [get, post & delete]
@@ -48,7 +76,9 @@ async def root():  # Function
 
 @app.get("/post")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"message": "Post retrive Successfully", "data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
@@ -89,6 +119,9 @@ def delete_post(id: int):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exists.",
         )
+        # SELECT * FROM products WHERE name LIKE '%en%';
+        # SELECT * FROM products WHERE price > 10 LIMIT 5;
+        # SELECT * FROM products ORDER BY id LIMIT 5 OFFSET 2;
 
     my_posts.pop(idx)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
